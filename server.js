620d77333c5f59e790238ea801ecc794fec61caa
Sonu Acharya <sonuacharya2022@gmail.com>
2024-06-04 13:39:28 +0530

    User model added

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION.Shutting down.....");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
@@ -7,18 +14,25 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {})
-  .then(() => {
-    console.log("DB connection successful");
-  });
+mongoose.connect(DB, {}).then(() => {
+  console.log("DB connection successful");
+});
 
 const app = require("./app");
 const PORT = 3000 || process.env.PORT;
 
 console.log(`Environment : ${app.get("env")}`);
+
 //START SERVER
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION.Shutting down.....");
+  server.close(() => {
+    process.exit(1);
+  });
 });
