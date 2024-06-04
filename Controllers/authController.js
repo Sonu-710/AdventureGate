6bc673fab1af71b22d751c9be81d13c151050a12
Sonu Acharya <sonuacharya2022@gmail.com>
2024-06-04 21:58:24 +0530

    reset password added

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -3,6 +3,7 @@ const User = require("./../Models/users");
 const catchAsync = require("./../utils/CatchAsync.js");
 const AppError = require("./../utils/AppError");
 const sendEmail = require("./../utils/email");
+const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 
 const signToken = (id) => {
@@ -108,6 +109,32 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new AppError("Invalid Token", 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
+
+
 //To-do
 //Test the password changed Property
