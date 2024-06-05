27d1d37e61050d31e269ffe98d3981464a3e82ab
Sonu Acharya <sonuacharya2022@gmail.com>
2024-06-05 21:40:04 +0530

    added few security measures

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -14,11 +14,26 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  user.password = undefined;
+  res.cookie("jwt", token, cookieOptions);
   res.status(statusCode).json({
     status: "success",
     token,
+    data: {
+      user,
+    },
   });
 };
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
   createSendToken(newUser, 201, res);
@@ -133,7 +148,11 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 exports.updateMyPassword = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.user._id).select("+password");
 
-  if (!req.body.currentPassword || !req.body.passwordConfirm || !req.body.newPassword)
+  if (
+    !req.body.currentPassword ||
+    !req.body.passwordConfirm ||
+    !req.body.newPassword
+  )
     return next(new AppError("Invalid Input", 404));
 
   const isPasswordCorrect = await user.correctPassword(
--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./Controllers/errorController");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+const app = express();
 
 const tourRouter = require("./Routes/tours");
 const userRouter = require("./Routes/users");
 
 //MIDDLEWARES
+app.use(helmet());
+
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP.Please try agian after some time",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
 app.use(express.static("public"));
 
 //ROUTES
